b516f081fa41d827923fb2b5eff0f12a95601536
saalmansourav6122 <saalmansourav111@gmail.com>
2025-01-02 19:34:47 +0600

    done

--- TimeoutCancellation/index.js
+++ TimeoutCancellation/index.js
@@ -1,16 +1,16 @@
 let cancellable = (fn, args, t) => {
-  let timeOutID;
-
   const cancelFn = () => {
     clearTimeout(timeOutID);
+    console.log("Timeout canceled");
   };
 
-  timeOutID = setTimeout(() => {
-    fn(...args);
+  let timeOutID = setTimeout(() => {
+    const result = fn(...args); // Call fn with arguments and capture result
+    console.log("Function executed:", result); // Log the result
   }, t);
 
   return cancelFn;
 };
 const fn1 = (x) => x * 5;
-const cancelFn1 = cancellable(fn1, [2], 20);
-setTimeout(cancelFn1, 50);
+const cancelFn1 = cancellable(fn1, [2], 2000);
+setTimeout(cancelFn1, 1000);
